a2da91acb79a6eb888d3e89a2fe687b665f28c00
maryam22001 <129103068+maryam22001@users.noreply.github.com>
2024-12-08 23:20:42 +0200

    [14] [Time Beats: 100.00%] [Memory Beats: 48.24%] - LeetPush

--- ArrayHashing/14-Longest-Common-Prefix.java
+++ ArrayHashing/14-Longest-Common-Prefix.java
@@ -1,4 +1,4 @@
-class Solution {
+/**class Solution {
     public String longestCommonPrefix(String[] strs) {
            if (strs == null || strs.length == 0) return \\;
            String prefix = strs[0];
@@ -14,4 +14,34 @@ class Solution {
               return prefix;
   
     }
+}**/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null ||strs.length==0){return \\;}
+        String prefix= strs[0];
+           // Compare the prefix with each subsequent word
+           //
+        for (int i = 1; i < strs.length; i++) {
+            // Update the prefix by comparing it with the current word
+            /**The startsWith method is a part of the String class in Java. 
+            It checks whether a given string starts with a specific prefix. */
+            while (!strs[i].startsWith(prefix)) {
+
+                // Reduce the prefix by removing the last character
+                /**Method: substring(int beginIndex, int endIndex)
+                  This method is a part of the String class in Java and is used to 
+                    extract a portion of a string. */
+                prefix = prefix.substring(0, prefix.length() - 1);
+                
+                // If prefix becomes empty, there's no common prefix
+                if (prefix.isEmpty()) {
+                    return \\;
+                }
+            }
+        }
+        
+        return prefix; 
+  
+    }
 }
+
