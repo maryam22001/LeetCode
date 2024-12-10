4bbc95d08455bb28ec2abe1b8c80b1c9dbc03a64
maryam22001 <129103068+maryam22001@users.noreply.github.com>
2024-12-10 18:50:50 +0200

    [344] [Time Beats: 100.00%] [Memory Beats: 18.67%] - LeetPush

--- LinkedLists/344-Reverse-String.java
+++ LinkedLists/344-Reverse-String.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public void reverseString(char[] s) {
         // Convert the char array to a String
         String string = new String(s);
@@ -9,6 +9,20 @@ class Solution {
         // Convert the reversed String back to a char array and update the original array
         for (int i = 0; i < s.length; i++) {
             s[i] = reversedString.charAt(i);
+        }
+    }
+}*/
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+         
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
         }
     }
 }
